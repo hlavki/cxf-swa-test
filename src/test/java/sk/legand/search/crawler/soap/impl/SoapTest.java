21ed3b2df7c60a7690e6279604d7046073f2cf7c
Patrik Martinovský <p4km9y@xit.camp>
2019-03-21 17:04:26 +0100

    cxf server fix

--- src/test/java/sk/legand/search/crawler/soap/impl/SoapTest.java
+++ src/test/java/sk/legand/search/crawler/soap/impl/SoapTest.java
@@ -1,15 +1,18 @@
 package sk.legand.search.crawler.soap.impl;
 
 import java.net.URL;
+
 import javax.activation.DataHandler;
 import javax.activation.FileDataSource;
-import javax.xml.namespace.QName;
+import javax.xml.ws.Binding;
 import javax.xml.ws.BindingProvider;
-import org.apache.cxf.endpoint.Server;
-import org.apache.cxf.jaxws.JaxWsServerFactoryBean;
+import javax.xml.ws.Endpoint;
+import javax.xml.ws.soap.SOAPBinding;
+
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.junit.Test;
+
 import sk.legand.search.crawler.soap.AddEntry;
 import sk.legand.search.crawler.soap.CrawlerService;
 import sk.legand.search.crawler.soap.CrawlerServicePort;
@@ -17,33 +20,20 @@ import sk.legand.search.crawler.soap.InjectionEntry;
 
 public class SoapTest {
 
-    static URL wsdlURL;
-    static QName serviceName;
-    static QName portName;
+    private static Endpoint ep;
 
-    static {
-        serviceName = new QName("http://search.legand.sk/crawler/soap/",
-                "CrawlerService");
-        portName = new QName("http://search.legand.sk/crawler/soap/", "CrawlerServicePort");
-    }
-
-    private static Server server;
 
     @BeforeClass
     public static void setUp() throws Exception {
-        JaxWsServerFactoryBean srv = new JaxWsServerFactoryBean();
-        String address = "http://localhost:8181/cxf/soap";
-        srv.setAddress(address);
-        srv.setServiceClass(CrawlerServicePort.class);
-        srv.setServiceBean(new CrawlerServiceImpl());
-        srv.setWsdlLocation("/META-INF/wsdl/crawler.wsdl");
-        server = srv.create();
+        ep = Endpoint.publish("http://localhost:8181/cxf/soap", new CrawlerServiceImpl());
+        Binding binding = ep.getBinding();
+        ((SOAPBinding)binding).setMTOMEnabled(true);
     }
 
     @AfterClass
     public static void tearDown() {
         try {
-            server.stop();
+            ep.stop();
         } catch (Throwable t) {
             System.out.println("Error thrown: " + t.getMessage());
         }
@@ -51,8 +41,8 @@ public class SoapTest {
 
     @Test
     public void base() throws Exception {
+        
         CrawlerServicePort port = new CrawlerService(new URL("http://localhost:8181/cxf/soap?wsdl")).getCrawlerServicePort();
-
         BindingProvider bp = (BindingProvider) port;
         bp.getRequestContext().put(BindingProvider.ENDPOINT_ADDRESS_PROPERTY, "http://localhost:8181/cxf/soap");
 
